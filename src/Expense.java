68b4d72fe07b93b1b5c5d95587286afa1b535519
Durgaprasad2220322 <kothurudurgaprasad7@gmail.com>
2025-12-18 15:50:39 +0530

    Update Expense.java

--- src/Expense.java
+++ src/Expense.java
@@ -1,1 +1,40 @@
+package src;
+import java.util.List;
+
+public class Expense{
+    private String expenseId;
+    private String description;
+    private double amount;
+    private User paidBy;
+    private List<Split> splits;
+
+    public Expense(String expenseId, String description, double amount,
+                   User paidBy, List<Split> splits){
+        this.expenseId = expenseId;
+        this.description = description;
+        this.amount = amount;
+        this.paidBy = paidBy;
+        this.splits = splits;
+    }
+
+    public String getExpenseId(){
+        return expenseId;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public double getAmount(){
+        return amount;
+    }
+
+    public User getPaidBy(){
+        return paidBy;
+    }
+
+    public List<Split> getSplits(){
+        return splits;
+    }
+}
 
