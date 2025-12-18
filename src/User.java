81d7a91a5d1d216d22c89b56cf3c0113928799ec
Durgaprasad2220322 <kothurudurgaprasad7@gmail.com>
2025-12-18 15:06:02 +0530

    Update User.java

--- src/User.java
+++ src/User.java
@@ -1,1 +1,20 @@
+package src;
+
+public class User {
+    private String UId;
+    private String Uname;
+
+    public User(String UId, String Uname) {
+        this.UId = UId;
+        this.Uname = Uname;
+    }
+
+    public String getUId() {
+        return UId;
+    }
+
+    public String getUname() {
+        return Uname;
+    }
+}
 
